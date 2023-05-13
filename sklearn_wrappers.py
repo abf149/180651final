5d7f425e668ae8f608132303e897f9118b50dc79
Xu Tang <xutang@g.harvard.edu>
2023-05-12 23:29:44 -0400

    Add activations

--- sklearn_wrappers.py
+++ sklearn_wrappers.py
@@ -110,8 +110,8 @@ def autoencoder_exp(X_train, X_test, k):
     loss = 'mse'
 
     input_layer = tf.keras.Input(shape=(input_dim,), name='input') # input layer
-    encoding_layer = tf.keras.layers.Dense(encoding_dim, name='encoding')(input_layer) # encoding layer
-    decoding_layer = tf.keras.layers.Dense(output_dim, name='decoding')(encoding_layer) # decoding layer
+    encoding_layer = tf.keras.layers.Dense(encoding_dim, activation='relu', name='encoding')(input_layer) # encoding layer
+    decoding_layer = tf.keras.layers.Dense(output_dim, activation='sigmoid', name='decoding')(encoding_layer) # decoding layer
 
     autoencoder = tf.keras.Model(input_layer, decoding_layer)
     autoencoder.compile(optimizer=optimizer, loss=loss)
